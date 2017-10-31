ced0048fd9abe6e63f2b3bd4ea3623a799022786
Kyle <kiloloco@me.com>
2017-10-30 20:28:16 -0700

    Initial Commit

---
+++ JustWork/JustWork.h
@@ -0,0 +1,19 @@
+//
+//  JustWork.h
+//  JustWork
+//
+//  Created by Kyle Lee on 10/30/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JustWork.
+FOUNDATION_EXPORT double JustWorkVersionNumber;
+
+//! Project version string for JustWork.
+FOUNDATION_EXPORT const unsigned char JustWorkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JustWork/PublicHeader.h>
+
+
