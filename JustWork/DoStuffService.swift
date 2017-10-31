b5798b23bbec45f0851e00f6a94997802372471b
Kyle <kiloloco@me.com>
2017-10-30 20:30:52 -0700

    Finished functionality

---
+++ JustWork/DoStuffService.swift
@@ -0,0 +1,19 @@
+//
+//  DoStuffService.swift
+//  JustWork
+//
+//  Created by Kyle Lee on 10/30/17.
+//  Copyright © 2017 Kilo Loco. All rights reserved.
+//
+
+import Foundation
+
+public class DoStuffService {
+    
+    private init() {}
+    
+    public static func awwYea() -> String {
+        return "AWWWWWWWWWWW YEEEEEEAAAAAAA"
+    }
+    
+}
